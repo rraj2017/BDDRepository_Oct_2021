29ad844fb49a98ff818c275d01a92981db248fba
Ritu Raj <rituraj@Ritus-MacBook-Pro.local>
2021-10-04 16:13:17 +0530

    added browser support class file

---
+++ src/main/java/com/testautomation/util/BrowserSupport.java
@@ -0,0 +1,10 @@
+package com.testautomation.util;
+
+public class BrowserSupport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("chrome browser");
+	}
+
+}

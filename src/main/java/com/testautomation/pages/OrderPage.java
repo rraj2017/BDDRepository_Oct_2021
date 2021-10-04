df5e7ed59d914d21c9014fdb30fc0d9a08351386
Ritu Raj <rituraj@Ritus-MacBook-Pro.local>
2021-10-04 23:16:13 +0530

    order page updated after conflict

--- src/main/java/com/testautomation/pages/OrderPage.java
+++ src/main/java/com/testautomation/pages/OrderPage.java
@@ -2,11 +2,8 @@ package com.testautomation.pages;
 
 public class OrderPage {
 	public void getOrder() {
-<<<<<<< HEAD
 		System.out.println("get order---naveen");
-=======
-		System.out.println("get order---xyz");
->>>>>>> 9d1b7a6e3d6f4158575a57a52a125201216173fb
+
 	}
 
 }
